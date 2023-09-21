277c3791080ca4f3c4f07a45352cf47ba08cffa7
hyewon218 <won_11111@naver.com>
2023-09-21 23:00:03 +0900

    init commit

---
+++ src/main/java/jpabook/kimjpa2/KimJpa2Application.java
@@ -0,0 +1,13 @@
+package jpabook.kimjpa2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimJpa2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KimJpa2Application.class, args);
+    }
+
+}
---
+++ src/test/java/jpabook/kimjpa2/KimJpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.kimjpa2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimJpa2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
